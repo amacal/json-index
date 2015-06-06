8223e8b2ff545c08237f3eafc834658d6eeae904
Adrian Macal <adma@amacal.pl>
2015-06-06 12:49:46 +0200

    Initialize the solution structure.

---
+++ src/JsonIndex/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JsonIndex")]
+[assembly: AssemblyProduct("JsonIndex")]
+[assembly: AssemblyCopyright("Copyright © Adrian Macal 2015")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
